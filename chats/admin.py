db7ff0c49ead8140dc48dbfd838557435fb49dc8
Rofix <wourkout123@gmail.com>
2024-12-28 15:42:50 +0100

    Create Chat Room Model and Register with Admin Panel - Developed `ChatRoom` model for chat functionality. - Registered `ChatRoom` model in the Django admin panel for management.

--- chats/admin.py
+++ chats/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import ChatRoom
+
+
 # Register your models here.
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "created_at")
+    search_fields = ("id", "name", "slug")
+    ordering = ("-id",)
--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,17 @@
+from autoslug import AutoSlugField
 from django.db import models
 
+
 # Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from="name", unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "Chat Room"
+        verbose_name_plural = "Chat Rooms"
+
+    def __str__(self):
+        return self.name
