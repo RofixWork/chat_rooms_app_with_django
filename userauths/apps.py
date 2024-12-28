1cba9f38a1cbcf3c046d8f772b78ed2c6d53dcc4
Rofix <wourkout123@gmail.com>
2024-12-28 14:59:57 +0100

    Move Signals from Chats App to Userauths App - Initially forgot to create signals inside the chats app. - Created and moved signals to handle user and profile updates into the userauths app.

--- userauths/apps.py
+++ userauths/apps.py
@@ -5,3 +5,6 @@ class UserauthsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "userauths"
     verbose_name = "User Authentication"
+
+    def ready(self):
+        import userauths.signals
